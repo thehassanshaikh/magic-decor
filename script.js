ca8ef76109640bbb6b7bfb6a4b27290c382ddf83
thehassanshaikh <thehasanshaikh@gmail.com>
2023-07-25 20:04:45 +0530

    added owl corusel slider

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
+    dots:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:5
+            items:1
         }
     }
 })
